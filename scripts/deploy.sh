431f1f7052658f4a97af34c37abe2cddd433953c
Andrew Holman <andrewholman@hotmail.com>
2018-10-21 01:10:45 -0700

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,10 @@ if (( $# != 1 )); then
     echo "Must specify a deploy branch"
 elif [ "$1" = "stage" ]; then
     echo "Deploying master to stage"
-    git push origin master:stage
+    git push origin stage
 elif [ "$2" = "production" ]; then
     echo "Deploying stage to production"
-    git push origin stage:production
+    git push origin production
 else
     echo "Unknow deploy command"
 fi
