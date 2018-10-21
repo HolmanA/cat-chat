4ac74449c9f1289133200aafa3368b62789e1db3
Andrew Holman <andrewholman@hotmail.com>
2018-10-21 01:39:38 -0700

    Update again

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,15 @@ if (( $# != 1 )); then
     echo "Must specify a deploy branch"
 elif [ "$1" = "stage" ]; then
     echo "Deploying master to stage"
+    git fetch origin
+    git checkout stage
+    git checkout master
     git merge master stage
 elif [ "$2" = "production" ]; then
     echo "Deploying stage to production"
+    git fetch origin
+    git checkout production
+    git checkout stage
     git merge stage production
 else
     echo "Unknow deploy command"
